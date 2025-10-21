62080bfbe0b05ec40628abe6c45d4b59bad2f296
Ayisha humee <ayishahumee@outlook.com>
2025-10-20 20:48:56 -1200

    added textfield for search p

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,13 +7,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("STORY TIME "),
+        title: Text(" SMART KIDS. "),
         centerTitle: true,
         leading: Icon(Icons.menu),
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 16.0),
+            padding: const EdgeInsets.only(right: 12.0),
             child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: "Search topic or story",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red.shade300),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
           ),
         ],
       ),
