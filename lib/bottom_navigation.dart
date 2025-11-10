76b07c2a455bbfa2c52bc8b9282f0b25f685824f
Ayisha humee <ayishahumee@outlook.com>
2025-11-09 21:08:58 -1200

    update:smart kid new version

--- lib/bottom_navigation.dart
+++ lib/bottom_navigation.dart
@@ -1,5 +1,31 @@
+// import 'package:flutter/material.dart';
+// import 'package:learningdart/home_page.dart';
+
+// class BottomNavigation extends StatefulWidget {
+//   const BottomNavigation({super.key});
+
+//   @override
+//   State<BottomNavigation> createState() => _BottomNavigationState();
+// }
+
+// class _BottomNavigationState extends State<BottomNavigation> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: HomePage(),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: [
+//           BottomNavigationBarItem(label: "Home", icon: Icon(Icons.star)),
+//           BottomNavigationBarItem(label: "celebration", icon: Icon(Icons.cake)),
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:learningdart/contact_page.dart';
 import 'package:learningdart/home_page.dart';
+import 'package:learningdart/profile_page.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({super.key});
@@ -9,14 +35,28 @@ class BottomNavigation extends StatefulWidget {
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
+  int indexToBeShown = 0;
+
+  var pages = [HomePage(), ContactPage(), ProfilePage()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomePage(),
+      body: pages[indexToBeShown],
+
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: indexToBeShown,
+        onTap: (value) {
+          setState(() {});
+          indexToBeShown = value;
+        },
         items: [
-          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.star)),
-          BottomNavigationBarItem(label: "celebration", icon: Icon(Icons.cake)),
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "profile", icon: Icon(Icons.person)),
+          BottomNavigationBarItem(
+            label: "contact",
+            icon: Icon(Icons.contact_page),
+          ),
         ],
       ),
     );
